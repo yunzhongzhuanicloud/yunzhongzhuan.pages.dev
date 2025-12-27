abb6fb421c4492cc561c517e77528e36e6dcbd95
yunzhongzhuanicloud <fxpxvz4gth@privaterelay.appleid.com>
2025-12-28 03:44:38 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6486,7 +6486,7 @@ login_input_button_login.onclick = function(){
 				}else{
 
 					// 批量注册的账号或滥用服务的账号禁止继续登录，阻止滥用服务者尝试继续使用本站服务。
-					if(ResultJSON["status_code"]!=undefined && (ResultJSON["status_code"] == 403 || ResultJSON["status_code"]==404)){
+					if(ResultJSON["status_code"]!=undefined && ResultJSON["status_code"] == 403 ){
 
 						const arr = [];
 						while (true) {
@@ -11233,6 +11233,7 @@ if(true||navigator.language.toLowerCase().indexOf('cn')!=-1){
 		item.style.display="";
 	}
 }
+
 
 
 
